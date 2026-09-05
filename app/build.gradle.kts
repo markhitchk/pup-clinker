9eec07a6eb8b5b57075223bed53196e1ba225338
markhitchk <mark.hitchk@gmail.com>
2026-09-05 05:38:11 -0700

    Bump Puppy Clicker to v1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.harleytg.puppyclicker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
