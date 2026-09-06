1916b60b86e8fb2d52bbfecbe75a5a00aee371d9
markhitchk <mark.hitchk@gmail.com>
2026-09-05 17:40:56 -0700

    Fix puppy roster crash and bump v1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.harleytg.puppyclicker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.7.0"
+        versionCode = 14
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
