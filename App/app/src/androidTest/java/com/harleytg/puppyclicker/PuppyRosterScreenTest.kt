7ee0945ee147df201d50412f54d4edf291c62611
markhitchk <mark.hitchk@gmail.com>
2026-09-10 03:01:41 -0700

    test: fix roster compose assertion import

--- App/app/src/androidTest/java/com/harleytg/puppyclicker/PuppyRosterScreenTest.kt
+++ App/app/src/androidTest/java/com/harleytg/puppyclicker/PuppyRosterScreenTest.kt
@@ -2,7 +2,6 @@ package com.harleytg.puppyclicker
 
 import android.app.Application
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
