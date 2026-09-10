77ef3fa07353db91871a9a6feec275380994e215
markhitchk <mark.hitchk@gmail.com>
2026-09-10 11:33:12 -0700

    Test HarleyTG public identity alias resolution

--- App/app/src/test/java/com/harleytg/puppyclicker/PuppyExchangeIdentityTest.kt
+++ App/app/src/test/java/com/harleytg/puppyclicker/PuppyExchangeIdentityTest.kt
@@ -38,4 +38,24 @@ class PuppyExchangeIdentityTest {
     fun usernameNormalizationRemainsIndependentOfExchangeIdentity() {
         assertEquals("harleytg", PuppyPlayerIdentity.normalizeUsername(" HarleyTG "))
     }
+    @Test
+    fun reservedHarleyTgPublicAliasesResolveToCanonicalIdentity() {
+        assertEquals(
+            "PC-HARLEYTG-DEV-0001",
+            PuppyPlayerIdentity.displayPlayerId("PC-5AD7F57F80FBE69809F96AEA963E2426")
+        )
+        assertEquals(
+            "PUP-HTG-DEV-0001",
+            PuppyPlayerIdentity.displayFriendCode("PUP-5XUV-SGZB-S9CX")
+        )
+        assertEquals(
+            "PUP-5XUV-SGZB-S9CX",
+            PuppyPlayerIdentity.resolveFriendCodeInput("PUP-HTG-DEV-0001")
+        )
+        assertTrue(PuppyPlayerIdentity.isValidFriendCodeInput("PUP-HTG-DEV-0001"))
+        assertTrue(PuppyPlayerIdentity.isValidPlayerId("PC-5AD7F57F80FBE69809F96AEA963E2426"))
+        assertTrue(PuppyPlayerIdentity.isValidFriendCode("PUP-5XUV-SGZB-S9CX"))
+        assertFalse(PuppyPlayerIdentity.isValidPlayerId("PC-HARLEYTG-DEV-0001"))
+    }
+
 }
