9410b138e97e5175002e7657e3619bf9253c03a3
markhitchk <mark.hitchk@gmail.com>
2026-09-09 21:59:37 -0700

    test: add JVM JSON implementation

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -313,6 +313,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.json:json:20260814")
 }
 
 apply(from = rootProject.file("tools/seasonal.gradle.kts"))
