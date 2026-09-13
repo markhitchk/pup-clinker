b17410a8cd9c8937ea4b51aab720fcb330d839c6
markhitchk <mark.hitchk@gmail.com>
2026-09-12 23:22:22 -0700

    Add AndroidX instrumentation runner dependency

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -327,6 +327,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     implementation("androidx.core:core-ktx:1.15.0")
