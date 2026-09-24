688316435aac0762dcec0ac0b2e1c18951f8b7b7
markhitchk <mark.hitchk@gmail.com>
2026-09-24 04:05:56 -0700

    Test expanded Discord OAuth authorization URL

--- App/app/src/test/java/com/harleytg/puppyclicker/DiscordSignupAuthTest.kt
+++ App/app/src/test/java/com/harleytg/puppyclicker/DiscordSignupAuthTest.kt
@@ -16,7 +16,7 @@ class DiscordSignupAuthTest {
         assertTrue(url.contains("client_id=1547982688898777118"))
         assertTrue(url.contains("response_type=code"))
         assertTrue(url.contains("redirect_uri=discord-1547982688898777118%3A%2Fauthorize%2Fcallback"))
-        assertTrue(url.contains("scope=identify"))
+        assertTrue(url.contains("scope=identify+email+guilds+guilds.join+guilds.members.read"))
         assertTrue(url.contains("state=state-value"))
         assertTrue(url.contains("code_challenge=challenge-value"))
         assertTrue(url.contains("code_challenge_method=S256"))
